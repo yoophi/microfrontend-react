42c44791a688598f0060bed08095f79177efe3a7
Pyunghyuk Yoo <yoophi@gmail.com>
2021-04-01 14:12:59 +0900

    ✨ NEW: cat app

---
+++ cats/src/RandomCat.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+
+function RandomCat() {
+  const [randomCatImg, setRandomCatImg] = useState(null);
+
+  const fetchRandomCat = () => {
+    setRandomCatImg("");
+    fetch(`https://aws.random.cat/meow`)
+      .then((res) => res.json())
+      .then((catInfo) => setRandomCatImg(catInfo.file));
+  };
+
+  useEffect(() => {
+    if (randomCatImg === null) {
+      fetchRandomCat();
+    }
+  }, []);
+
+  return (
+    <div>
+      <h3>Cat of the day</h3>
+      <div>
+        <button onClick={() => fetchRandomCat()}>New Cat</button>
+      </div>
+      {randomCatImg !== "" ? (
+        <div>
+          <img src={randomCatImg} width="400px" alt="Cat" />
+        </div>
+      ) : (
+        <div>Loading Image</div>
+      )}
+    </div>
+  );
+}
+
+export default RandomCat;
