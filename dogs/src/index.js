156d5756b02e22482c4c3b58ca80f70c069b62ef
Pyunghyuk Yoo <yoophi@gmail.com>
2021-04-01 20:43:23 +0900

    ✨ NEW: add mount/unmount methods for cats-app and dogs-app

--- dogs/src/index.js
+++ dogs/src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+window.renderDogs = (containerId, history) => {
+  ReactDOM.render(
+    <App history={history} />,
+    document.getElementById(containerId)
+  );
+};
+
+window.unmountDogs = (containerId) => {
+  ReactDOM.unmountComponentAtNode(document.getElementById(containerId));
+};
+
+if (!document.getElementById("Dogs-container")) {
+  ReactDOM.render(<App />, document.getElementById('root'))
+}
