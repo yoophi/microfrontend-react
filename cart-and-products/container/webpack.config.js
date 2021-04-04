f097fecad2061c4d0a69ec2b90da4ad426eef950
Pyunghyuk Yoo <yoophi@gmail.com>
2021-04-04 18:47:24 +0900

    ✨ IMPROVE: apply module federation

--- cart-and-products/container/webpack.config.js
+++ cart-and-products/container/webpack.config.js
@@ -1,4 +1,5 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
 module.exports = {
   mode: "development",
@@ -6,8 +7,15 @@ module.exports = {
     port: 8080,
   },
   plugins: [
+    new ModuleFederationPlugin({
+      name: "container",
+      remotes: {
+        products: "products@http://localhost:8081/remoteEntry.js",
+        cart: "cart@http://localhost:8082/remoteEntry.js",
+      },
+    }),
     new HtmlWebpackPlugin({
-      template: './public/index.html',
+      template: "./public/index.html",
     }),
   ],
 };
