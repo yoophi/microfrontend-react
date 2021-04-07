b85d10c6da3f87e4aa2f44ef4c9007f8e252dabf
Pyunghyuk Yoo <yoophi@gmail.com>
2021-04-07 19:49:33 +0900

    ✨ IMPROVE: federate with mount function

--- cart-and-products/cart/src/bootstrap.js
+++ cart-and-products/cart/src/bootstrap.js
@@ -1,5 +1,16 @@
 import faker from "faker";
 
-const cartText = `<div>You have ${faker.random.number()} items in your cart</div>`;
+const mount = (el) => {
+  const cartText = `<div>You have ${faker.random.number()} items in your cart</div>`;
 
-document.querySelector("#dev-carts").innerHTML = cartText;
+  el.innerHTML = cartText;
+};
+
+if (process.env.NODE_ENV === "development") {
+  const el = document.querySelector("#dev-carts");
+  if (el) {
+    mount(el);
+  }
+}
+
+export { mount };
--- cart-and-products/container/src/bootstrap.js
+++ cart-and-products/container/src/bootstrap.js
@@ -1,4 +1,7 @@
-import 'products/ProductsIndex';
-import 'cart/CartShow';
+import { mount as mountCart } from "cart/CartShow";
+import { mount as mountProduct } from "products/ProductsIndex";
 
-console.log('Container!');
+console.log("Container!");
+
+mountProduct(document.querySelector("#my-dev-products"));
+mountCart(document.querySelector("#my-dev-carts"));
